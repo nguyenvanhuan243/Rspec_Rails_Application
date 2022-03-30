996de296795c8768f540f5adf8db177631e1fc45
Huan Nguyen <huan.nguyen@deliveree.com>
2022-03-30 18:13:58 +0700

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220330105310) do
+ActiveRecord::Schema.define(version: 20220330110224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20220330105310) do
   end
 
   create_table "ethereums", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "nears", force: :cascade do |t|
     t.string   "name"
     t.float    "price"
     t.datetime "created_at", null: false
